e7f6f7f56c8dd0912b3e04dff302b8d77bcb3a77
Gustavo Leal de Borba <73996@PROWAY.TREINA>
2025-02-12 19:23:44 -0300

    Criando Componete Student

--- sistema-aluno-proway/src/app/app.component.ts
+++ sistema-aluno-proway/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'sistema-aluno-proway';
+  title = 'Sistema Aluno Proway';
 }
